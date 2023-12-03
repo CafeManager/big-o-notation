9322dbdeb823bba6763baaeaa3e7061f6d7c3792
CafeManager <kevinflores1997@yahoo.com>
2023-12-03 15:46:15 -0800

    added tree assignment

--- 06-tree/binary-tree.js
+++ 06-tree/binary-tree.js
@@ -16,22 +16,141 @@ class BinaryTree {
     /** minDepth(): return the minimum depth of the tree -- that is,
      * the length of the shortest path from the root to a leaf. */
 
-    minDepth() {}
+    minDepth() {
+        if (this.root == null) {
+            return 0;
+        }
+        let stack = [[this.root]];
+        let minLength = 1;
+        let children = [];
+        while (stack) {
+            let current = stack.pop();
+            for (let node of current) {
+                if (!node) {
+                    return minLength;
+                }
+                if (!node.left || !node.right) {
+                    return minLength;
+                } else {
+                    children.push(node.left);
+                    children.push(node.right);
+                }
+                minLength += 1;
+            }
+            stack.push(children);
+            children = [];
+        }
+        return minLength;
+    }
 
     /** maxDepth(): return the maximum depth of the tree -- that is,
      * the length of the longest path from the root to a leaf. */
 
-    maxDepth() {}
+    maxDepth() {
+        if (this.root == null) {
+            return 0;
+        }
+        let stack = [this.root];
+        let valList = [];
+        let minLength = 0;
+        let maxLength = 0;
+        while (stack.length > 0) {
+            let current = stack.shift();
+            if (current == null) {
+                minLength -= 1;
+                valList.pop();
+            } else {
+                if (current.left && current.right) {
+                    stack.unshift(current.right);
+                    stack.unshift(current.left);
+                    valList.push(current.val);
+                    minLength += 1;
+                    if (minLength > maxLength) {
+                        maxLength = minLength;
+                    }
+                } else if (current.left && !current.right) {
+                    stack.unshift(null);
+                    stack.unshift(current.left);
+                    valList.push(current.val);
+                    minLength += 1;
+                    if (minLength > maxLength) {
+                        maxLength = minLength;
+                    }
+                } else if (!current.left && current.right) {
+                    stack.push(current.right);
+                    valList.push(current.val);
+                    minLength += 1;
+                    if (minLength > maxLength) {
+                        maxLength = minLength;
+                    }
+                } else if (!current.left && !current.right) {
+                    minLength += 1;
+                    valList.push(current.val);
+                    if (minLength > maxLength) {
+                        maxLength = minLength;
+                    }
+                    valList.pop();
+                    minLength -= 1;
+                }
+            }
+        }
+        return maxLength;
+    }
 
     /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
      * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
-    maxSum() {}
+    maxSum() {
+        let max = 0;
+
+        let getMaxPath = (node) => {
+            if (!node) {
+                return 0;
+            }
+
+            let leftSum = getMaxPath(node.left);
+            let rightSum = getMaxPath(node.right);
+            let openMax = Math.max(
+                Math.max(leftSum, rightSum) + node.val,
+                node.val
+            );
+            let closedMax = Math.max(openMax, leftSum + rightSum + node.val);
+
+            if (closedMax > max) {
+                max = closedMax;
+            }
+            return openMax;
+        };
+        let tempMax = getMaxPath(this.root);
+        if (tempMax > max) {
+            return tempMax;
+        } else {
+            return max;
+        }
+    }
 
     /** nextLarger(lowerBound): return the smallest value in the tree
      * which is larger than lowerBound. Return null if no such value exists. */
 
-    nextLarger(lowerBound) {}
+    nextLarger(lowerBound) {
+        let next = null;
+        let stack = [this.root];
+
+        while (stack.length > 0) {
+            let node = stack.pop();
+            if (node) {
+                if (
+                    node.val > lowerBound &&
+                    (node.val < next || next == null)
+                ) {
+                    next = node.val;
+                }
+                stack.push(node.right);
+                stack.push(node.left);
+            }
+        }
+        return next;
+    }
 
     /** Further study!
      * areCousins(node1, node2): determine whether two nodes are cousins
@@ -55,5 +174,23 @@ class BinaryTree {
 
     lowestCommonAncestor(node1, node2) {}
 }
+
+emptyTree = new BinaryTree();
+
+// build small tree;
+let smallLeft = new BinaryTreeNode(5);
+let smallRight = new BinaryTreeNode(5);
+let smallRoot = new BinaryTreeNode(6, smallLeft, smallRight);
+smallTree = new BinaryTree(smallRoot);
+
+// build large tree
+let node6 = new BinaryTreeNode(1);
+let node5 = new BinaryTreeNode(1);
+let node4 = new BinaryTreeNode(2);
+let node3 = new BinaryTreeNode(3, node4, node6);
+let node2 = new BinaryTreeNode(5, node3, node5);
+let node1 = new BinaryTreeNode(5);
+let root = new BinaryTreeNode(6, node1, node2);
+largeTree = new BinaryTree(root);
 
 module.exports = { BinaryTree, BinaryTreeNode };
--- 06-tree/tree.js
+++ 06-tree/tree.js
@@ -15,17 +15,88 @@ class Tree {
     /** sumValues(): add up all of the values in the tree. */
 
     sumValues() {
-      
+        let stack = [this.root];
+        let sum = 0;
+        while (stack.length > 0) {
+            console.log(stack);
+            let current = stack.pop();
+            if (current) {
+                for (let child of current.children) {
+                    console.log(current.children);
+                    stack.push(child);
+                }
+                sum += current.val;
+            }
+        }
+        return sum;
     }
 
     /** countEvens(): count all of the nodes in the tree with even values. */
 
-    countEvens() {}
+    countEvens() {
+        let stack = [this.root];
+        let total = 0;
+        while (stack.length > 0) {
+            let current = stack.pop();
+            if (current) {
+                for (let child of current.children) {
+                    console.log(current.children);
+                    stack.push(child);
+                }
+                if (current.val % 2 == 0) {
+                    total += 1;
+                }
+            }
+        }
+        return total;
+    }
 
     /** numGreater(lowerBound): return a count of the number of nodes
      * whose value is greater than lowerBound. */
 
-    numGreater(lowerBound) {}
+    numGreater(lowerBound) {
+        let stack = [this.root];
+        let total = 0;
+        while (stack.length > 0) {
+            let current = stack.pop();
+            if (current) {
+                for (let child of current.children) {
+                    console.log(current.children);
+                    stack.push(child);
+                }
+                if (current.val > lowerBound) {
+                    total += 1;
+                }
+            }
+        }
+        return total;
+    }
 }
+
+// build small tree
+let nSmall = new TreeNode(1);
+let nSmall2 = new TreeNode(2);
+nSmall.children.push(nSmall2);
+smallTree = new Tree(nSmall);
+
+// build large tree
+let n = new TreeNode(1);
+let n2 = new TreeNode(2);
+let n3 = new TreeNode(3);
+let n4 = new TreeNode(4);
+let n5 = new TreeNode(5);
+let n6 = new TreeNode(6);
+let n7 = new TreeNode(7);
+let n8 = new TreeNode(8);
+
+n.children = [n2, n3, n4];
+
+n4.children.push(n5, n6);
+n6.children.push(n7);
+n7.children.push(n8);
+
+largeTree = new Tree(n);
+
+console.log(largeTree.sumValues());
 
 module.exports = { Tree, TreeNode };
